92aa6a57d6eaa0f61271b87055e8775a241260b6
unknown <daveasmah@yahoo.com>
2022-06-27 22:29:26 -0400

    First commit

---
+++ style.js
@@ -0,0 +1,29 @@
+//Changing color of background using "onclick" event
+
+
+//Declaring a variable  for the querySelector 
+let screen = document.querySelector('.screen');
+
+//Writing a function with an event listener
+screen.addEventListener('click', function(){
+	// When button is clicked on background color changes to "Grey".
+	screen.style.backgroundColor = "grey";	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
